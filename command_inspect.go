ac2cf07c4359d55ff79bcad84e4d3e757d76d090
ian <ianh@mac.com>
2025-04-25 20:08:48 -0700

    CH2L5: passes

--- command_inspect.go
+++ command_inspect.go
@@ -16,6 +16,19 @@ func commandInspect(cfg *config, args ...string) error {
     for _, pokemon := range cfg.characterPokedex {
         if pokemon.Name == name {
             fmt.Printf("Name: %s\n", pokemon.Name)
+            fmt.Printf("Height: %d\n", pokemon.Height)
+            fmt.Printf("Weight: %d\n", pokemon.Weight)
+            fmt.Println("Stats:")
+            pkStats := pokemon.Stats
+//            fmt.Printf("  -%vs\n", pkStats)
+            for _, name := range pkStats {
+                fmt.Printf("  -%s: %d\n", name.Stat.Name, name.BaseStat)
+            }
+            pkTypes := pokemon.Types
+            fmt.Println("Types:")
+            for _, types := range pkTypes {
+                fmt.Printf("  - %s\n", types.Type.Name)
+            }
         } else {
             fmt.Println("you have not caught that pokemon")
         }
