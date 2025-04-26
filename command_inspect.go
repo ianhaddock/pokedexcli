70186665513700a03dcf71b459a986da5d111d5b
ian <ianh@mac.com>
2025-04-25 20:17:17 -0700

    CH2L5: after review

--- command_inspect.go
+++ command_inspect.go
@@ -13,26 +13,24 @@ func commandInspect(cfg *config, args ...string) error {
 
     name := args[0]
 
-    for _, pokemon := range cfg.characterPokedex {
-        if pokemon.Name == name {
-            fmt.Printf("Name: %s\n", pokemon.Name)
-            fmt.Printf("Height: %d\n", pokemon.Height)
-            fmt.Printf("Weight: %d\n", pokemon.Weight)
-            fmt.Println("Stats:")
-            pkStats := pokemon.Stats
-//            fmt.Printf("  -%vs\n", pkStats)
-            for _, name := range pkStats {
-                fmt.Printf("  -%s: %d\n", name.Stat.Name, name.BaseStat)
-            }
-            pkTypes := pokemon.Types
-            fmt.Println("Types:")
-            for _, types := range pkTypes {
-                fmt.Printf("  - %s\n", types.Type.Name)
-            }
-        } else {
-            fmt.Println("you have not caught that pokemon")
-        }
+    pokemon, ok := cfg.characterPokedex[name]
+    if !ok {
+        return errors.New("you have not caught that pokemon")
     }
+
+    fmt.Println("Name:", pokemon.Name)
+    fmt.Printf("Height: %d\n", pokemon.Height)
+    fmt.Printf("Weight: %d\n", pokemon.Weight)
+    fmt.Println("Stats:")
+
+    for _, stat := range pokemon.Stats {
+        fmt.Printf("  -%s: %v\n", stat.Stat.Name, stat.BaseStat)
+    }
+   
+    fmt.Println("Types:")
+    for _, typeInfo := range pokemon.Types {
+        fmt.Println("  -", typeInfo.Type.Name)
+        }
     
     return nil
 }
